995855cfa31662d91e34cba61ecb7d4d0523f0b8
Darren Reid <darren@reidmail.org>
2022-06-13 15:31:41 +1000

    Test deployment with docker-compose healthcheck restore while hitting API and existing db file (most common deployment).

--- api/LitestreamViteTest.ServiceInterface/MyServices.cs
+++ api/LitestreamViteTest.ServiceInterface/MyServices.cs
@@ -8,7 +8,7 @@ namespace LitestreamViteTest.ServiceInterface
     {
         public object Any(Hello request)
         {
-            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+            return new HelloResponse { Result = $"Hello, {request.Name}!!!!11one" };
         }
     }
 }
